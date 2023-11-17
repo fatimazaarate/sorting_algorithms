d47bb73dfee6481a8ee6c3a3715446d161b39119
fatimazaarate <fatima.zaarate@gmail.com>
2023-11-17 12:43:10 +0100

    handling null case

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	int tmp;
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
